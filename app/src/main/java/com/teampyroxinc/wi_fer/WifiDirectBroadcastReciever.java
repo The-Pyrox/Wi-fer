be3fa61ae053ac76e7c4db86a23af92a3ab129c0
Pyrox <poojanrathod@gmail.com>
2016-08-26 23:25:18 +0530

    Initial commit

---
+++ app/src/main/java/com/teampyroxinc/wi_fer/WifiDirectBroadcastReciever.java
@@ -0,0 +1,48 @@
+package com.teampyroxinc.wi_fer;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.os.IBinder;
+
+/**
+ * Created by Poojan on 8/26/2016.
+ */
+public class WifiDirectBroadcastReciever extends BroadcastReceiver {
+
+    private WifiP2pManager mManager;
+    private WifiP2pManager.Channel mChannel;
+    private MainActivity mActivity;
+
+
+
+    public WifiDirectBroadcastReciever(WifiP2pManager manager, WifiP2pManager.Channel channel,
+                                       MainActivity activity) {
+        super();
+        this.mManager = manager;
+        this.mChannel = channel;
+        this.mActivity = activity;
+
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            // Check to see if Wi-Fi is enabled and notify appropriate activity
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            // Call WifiP2pManager.requestPeers() to get a list of current peers
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+            // Respond to new connection or disconnections
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            // Respond to this device's wifi state changing
+        }
+
+    }
+
+    @Override
+    public IBinder peekService(Context myContext, Intent service) {
+        return super.peekService(myContext, service);
+    }
+}
