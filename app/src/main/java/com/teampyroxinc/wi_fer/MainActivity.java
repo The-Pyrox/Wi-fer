314461a6fc543f91f3de6adef19ab373d93646a5
Pyrox <poojanrathod@gmail.com>
2016-08-29 18:52:44 +0530

    few changes

--- app/src/main/java/com/teampyroxinc/wi_fer/MainActivity.java
+++ app/src/main/java/com/teampyroxinc/wi_fer/MainActivity.java
@@ -1,25 +1,58 @@
 package com.teampyroxinc.wi_fer;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.IntentFilter;
+import android.database.DataSetObserver;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.*;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
 
 
     WifiP2pManager mManager;
     WifiP2pManager.Channel mChannel;
     BroadcastReceiver mReceiver;
     IntentFilter mIntentFilter;
+    WifiP2pManager.PeerListListener myPeerListListener;
+    private List<WifiP2pDevice> peers = new ArrayList<WifiP2pDevice>();
+    private WifiP2pDevice device;
+    private ArrayAdapter<WifiP2pDevice> adapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        myPeerListListener = new WifiP2pManager.PeerListListener() {
+            @Override
+            public void onPeersAvailable(WifiP2pDeviceList peerlist) {
+                peers.addAll(peerlist.getDeviceList());
+
+            }
+        };
+        adapter = new ArrayAdapter<WifiP2pDevice>(this,R.layout.device_list,peers);
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+
 
         mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
         mChannel = mManager.initialize(this, getMainLooper(), null);
@@ -35,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        mReceiver = new WifiDirectBroadcastReciever(mManager, mChannel, this);
         registerReceiver(mReceiver, mIntentFilter);
     }
 
@@ -44,7 +78,20 @@ public class MainActivity extends AppCompatActivity {
         unregisterReceiver(mReceiver);
     }
 
+    public void search() {
+        mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(MainActivity.this, "Discovery Initiated", Toast.LENGTH_SHORT).show();
+            }
 
-    @Override
+            @Override
+            public void onFailure(int reasonCode) {
+                Toast.makeText(MainActivity.this, "Discovery Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
 
 }
