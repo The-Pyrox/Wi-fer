0b549c20df9b0d068f38f5531b4ab01d5259f366
Pyrox <poojanrathod@gmail.com>
2016-09-22 22:09:08 +0530

    Atlast multiple files sent in one connection

--- app/src/main/java/com/teampyroxinc/wi_fer/ClientActivity.java
+++ app/src/main/java/com/teampyroxinc/wi_fer/ClientActivity.java
@@ -1,17 +1,12 @@
 package com.teampyroxinc.wi_fer;
 
-
-
-
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
@@ -22,6 +17,8 @@ public class ClientActivity extends AppCompatActivity  {
     private String host;
     public Bundle bundle;
     public  EditText textout;
+    public TextView display;
+    ClientAsyncTask clientAsyncTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,19 +27,25 @@ public class ClientActivity extends AppCompatActivity  {
         bundle = getIntent().getExtras();
         host = bundle.getString("Host");
         textout = (EditText)findViewById(R.id.textout);
+        display = (TextView)findViewById(R.id.display);
 
+
+    }
+    public void closetask(){
+        clientAsyncTask.cancel(true);
 
     }
 
     public void send_data(View view){
         byte[] buf = textout.getText().toString().getBytes();
         int len = textout.getText().length();
-        ClientAsyncTask clientAsyncTask = new ClientAsyncTask(host,buf,len);
+        clientAsyncTask = new ClientAsyncTask(host,buf,len);
         clientAsyncTask.execute();
 
 
+
     }
-    public static class ClientAsyncTask extends AsyncTask{
+    public class ClientAsyncTask extends AsyncTask{
         String host_address;
         byte[] buf;
         int len;
@@ -51,9 +54,9 @@ public class ClientActivity extends AppCompatActivity  {
             this.host_address = host;
             this.buf = buf;
             this.len = len;
-
-
         }
+
+
 
         @Override
         protected Object doInBackground(Object[] objects) {
@@ -62,7 +65,6 @@ public class ClientActivity extends AppCompatActivity  {
                 socket.bind(null);
                 socket.connect((new InetSocketAddress(host_address,8080)),5000);
                 OutputStream outputStream = socket.getOutputStream();
-
                 outputStream.write(buf, 0, len);
                 outputStream.close();
             } catch (IOException e) {
@@ -71,6 +73,12 @@ public class ClientActivity extends AppCompatActivity  {
 
 
             return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            closetask();
         }
     }
 
--- app/src/main/java/com/teampyroxinc/wi_fer/ServerActivity.java
+++ app/src/main/java/com/teampyroxinc/wi_fer/ServerActivity.java
@@ -3,6 +3,7 @@ package com.teampyroxinc.wi_fer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,17 +14,28 @@ import java.util.Arrays;
 public class ServerActivity extends AppCompatActivity {
     public Integer b;
     public TextView display;
+    public String v;
+    Button button;
+    boolean connected = true;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_server);
+        button = (Button) findViewById(R.id.button);
+        connected = true;
+
+        Thread socketServerThread = new Thread(new SocketServerThread());
+        display = (TextView) findViewById(R.id.display);
+        socketServerThread.start();
+    }
+    public void server_open(){
         Thread socketServerThread = new Thread(new SocketServerThread());
         socketServerThread.start();
-        display = (TextView)findViewById(R.id.display);
 
     }
+
     private class SocketServerThread extends Thread {
 
         static final int SocketServerPORT = 8080;
@@ -32,22 +44,26 @@ public class ServerActivity extends AppCompatActivity {
         @Override
         public void run() {
             try {
-                final byte buf[] = new byte[2];
-
+                final byte buf[] = new byte[1024];
                 ServerSocket serverSocket = new ServerSocket(SocketServerPORT);
-                Socket socket = serverSocket.accept();
-                InputStream inputStream = socket.getInputStream();
-                b =inputStream.read(buf);
-                final String v = new String(buf,"UTF-8");
+                while (connected == true) {
+                    Socket socket = serverSocket.accept();
+                    InputStream inputStream = socket.getInputStream();
+                    b = inputStream.read(buf);
+                    v = new String(buf, "UTF-8");
+                    connected = true;
+
+
 
                 ServerActivity.this.runOnUiThread(new Runnable() {
 
                     @Override
                     public void run() {
                         display.setText(v);
-
+                        server_open();
                     }
-                });
+                }
+                );}
             } catch (IOException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
